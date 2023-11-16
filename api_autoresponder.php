6304ab12e1b1c3a766d0aa5e09e3068695a6cb94
Banhodeouro18k <143032472+Banhodeouro18k@users.noreply.github.com>
2023-11-15 22:49:31 -0300

    Update api_autoresponder.php

--- api_autoresponder.php
+++ api_autoresponder.php
@@ -6,72 +6,71 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// add this php file to your web server and enter the complete url in AutoResponder (e.g. https://www.example.com/api_autoresponder.php)
-
-// to allow only authorized requests, you need to configure your .htaccess file and set the credentials with the Basic Auth option in AutoResponder
-
-// access a custom header added in your AutoResponder rule
-// replace XXXXXX_XXXX with the name of the header in UPPERCASE (and with '-' replaced by '_')
-$myheader = $_SERVER['HTTP_XXXXXX_XXXX'];
-  
 // get posted data
 $data = json_decode(file_get_contents("php://input"));
-  
+
 // make sure json data is not incomplete
-if(
-	!empty($data->query) &&
-	!empty($data->appPackageName) &&
-	!empty($data->messengerPackageName) &&
-	!empty($data->query->sender) &&
-	!empty($data->query->message)
-){
-	
-	// package name of AutoResponder to detect which AutoResponder the message comes from
-	$appPackageName = $data->appPackageName;
-	// package name of messenger to detect which messenger the message comes from
-	$messengerPackageName = $data->messengerPackageName;
-	// name/number of the message sender (like shown in the Android notification)
-	$sender = $data->query->sender;
-	// text of the incoming message
-	$message = $data->query->message;
-	// is the sender a group? true or false
-	$isGroup = $data->query->isGroup;
-	// name/number of the group participant who sent the message if it was sent in a group, otherwise empty
-	$groupParticipant = $data->query->groupParticipant;
-	// id of the AutoResponder rule which has sent the web server request
-	$ruleId = $data->query->ruleId;
-	// is this a test message from AutoResponder? true or false
-	$isTestMessage = $data->query->isTestMessage;
-	
-	
-	
-	// process messages here
-	
-	
-	
-	// set response code - 200 success
-	http_response_code(200);
+if (
+    !empty($data->query) &&
+    !empty($data->appPackageName) &&
+    !empty($data->messengerPackageName) &&
+    !empty($data->query->sender) &&
+    !empty($data->query->message)
+) {
 
-	// send one or multiple replies to AutoResponder
-	echo json_encode(array("replies" => array(
-		array("message" => "Hey " . $sender . "!\nThanks for sending: " . $message),
-		array("message" => "Success ✅")
-	)));
-	
-	// or this instead for no reply:
-	// echo json_encode(array("replies" => array()));
-}
+    // Extracting data from the received JSON
+    $appPackageName = $data->appPackageName;
+    $messengerPackageName = $data->messengerPackageName;
+    $sender = $data->query->sender;
+    $message = $data->query->message;
 
-// tell the user json data is incomplete
-else{
-	
-	// set response code - 400 bad request
-	http_response_code(400);
-	
-	// send error
-	echo json_encode(array("replies" => array(
-		array("message" => "Error ❌"),
-		array("message" => "JSON data is incomplete. Was the request sent by AutoResponder?")
-	)));
+    // Process messages here if needed
+
+    // Preparing data for the cURL request to the prediction API
+    $predictionData = array(
+        'question' => $message // Using the received message as the question
+    );
+
+    // cURL request to the prediction API endpoint
+    $predictionURL = 'https://germinal-flowise.hf.space/api/v1/prediction/ee8fc21f-7886-4df6-87c3-79be136fb5bf';
+    $ch = curl_init($predictionURL);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($predictionData));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Authorization: Bearer 8rGR/M0S9Y0ycHzrZ3lfDBbTsqziPYn9XxNOaG0J67w='
+    ));
+
+    // Execute cURL request and capture the response
+    $predictionResponse = curl_exec($ch);
+    $predictionHTTPCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    // Check if cURL request was successful
+    if ($predictionHTTPCode == 200) {
+        // Successfully received response from prediction API
+        // Process the prediction response here if needed
+
+        // Assuming no additional processing, preparing the response to AutoResponder
+        $autoResponderResponse = array(
+            "replies" => array(
+                array("message" => "Hey " . $sender . "!\nThanks for sending: " . $message),
+                array("message" => "Success ✅")
+            )
+        );
+
+        // Set response code - 200 success
+        http_response_code(200);
+        echo json_encode($autoResponderResponse);
+    } else {
+        // If there's an error in the cURL request to the prediction API
+        http_response_code($predictionHTTPCode);
+        echo json_encode(array("error" => "Failed to get prediction response"));
+    }
+} else {
+    // Tell the user json data is incomplete
+    http_response_code(400);
+    echo json_encode(array("error" => "JSON data is incomplete. Was the request sent by AutoResponder?"));
 }
 ?>
